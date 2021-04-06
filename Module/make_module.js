04328a8b5fcdbceafc20724faced95e6e26e53e6
Yoojeong Lee <yjclarelee@postech.ac.kr>
2021-04-06 13:27:35 +0900

    Use module syntax

---
+++ Module/make_module.js
@@ -0,0 +1,13 @@
+let odd = 1;
+let even = 2;
+
+module.exports = {
+    odd,
+    even
+}
+
+/**
+ * module.exports = odd
+ * module.exports = [odd, even]
+ * is possible
+ */
---
+++ Module/use_module.js
@@ -0,0 +1,4 @@
+const value = require("./make_module");
+// const {odd, even} = require("./make_module");
+
+console.log(value);
