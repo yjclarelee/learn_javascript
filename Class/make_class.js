d020bf48985a2fda4d405fc25fbf7a1d0e6cdd9d
Yoojeong Lee <yjclarelee@postech.ac.kr>
2021-04-07 18:34:04 +0900

    Correctly write classes

---
+++ Class/make_class.js
@@ -0,0 +1,28 @@
+class Clock{
+    primeNum = 5; 
+    constructor({ template }){
+        this.template = template
+    }
+    render(){
+        console.log(primeNum);
+    }
+    stop(){}
+    start(){}
+}
+
+
+class Animal{
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+class Rabbit extends Animal{
+    constructor(name) {
+        super(name);
+        this.created = Date.now();
+    }
+}
+
+let rabbit = new Rabbit("White rabbit");
+console.log(rabbit.name);
